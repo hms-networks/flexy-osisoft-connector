36b09550628a4a83bf89c234adbba120fdd5a2cd
TomKimsey <thk@hms.se>
2018-04-24 11:19:53 -0400

    Moved OSIsoft PI server functionality to its own class
    Cleans up the interface to an OSIsoft pi server.  Moving this functionality
    to a new class also introduces the possibility of supporting multiple servers
    if that is needed.

---
+++ OSIsoft_Demo/src/OSIsoftServer.java
@@ -0,0 +1,55 @@
+import com.ewon.ewonitf.EWException;
+import com.ewon.ewonitf.ScheduledActionManager;
+
+/**
+ * 
+ * Class object for an OSIsoft PI Server.
+ * 
+ * HMS Industrial Networks Inc. Solution Center
+ * 
+ * @author thk
+ *
+ */
+
+public class OSIsoftServer {
+
+   // IP address of OSIsoft Server
+   private String serverIP;
+
+   // BASE64 encoded BASIC authentication credentials
+   private String authCredentials;
+   
+   // URL for the OSIsoft Server
+   private String targetURL;
+
+   // Post Headers
+   private String postHeaders;
+   
+   public OSIsoftServer(String ip, String login) {
+      serverIP = ip;
+      authCredentials = login;
+      targetURL = "https://" + serverIP + "/piwebapi/streams/";
+      postHeaders = "Authorization=Basic " + authCredentials + "&Content-Type=application/json";
+   }
+   
+   
+   // Posts a tag value to the OSIsoft server
+   public void postTag(Tag tag) {
+      try {
+         ScheduledActionManager.RequestHttpX(targetURL + tag.getWebID() + "/Value", "Post", postHeaders,
+               buildBody(tag.getTagValue()), "", "");
+      } catch (EWException e) {
+         e.printStackTrace();
+      }
+   }
+   
+   // Builds and returns the body content
+   // Hardcoded JSON payload
+   private static String buildBody(String value) {
+      String jsonBody = "{\r\n" + "    \"Timestamp\": \"1970-01-01T00:00:00Z\",\r\n" + "    \"Value\": " + value
+            + ",\r\n" + "    \"UnitsAbbreviation\": \"\",\r\n" + "    \"Good\": true,\r\n"
+            + "    \"Questionable\": false,\r\n" + "}";
+      return jsonBody;
+   }
+   
+}
