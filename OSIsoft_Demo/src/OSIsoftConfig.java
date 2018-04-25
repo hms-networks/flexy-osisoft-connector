9a515f59cc83c18609a71dc1d45047820124c29e
TomKimsey <thk@hms.se>
2018-04-25 12:35:42 -0400

    Created OSIsoftConfig.java
    This class will contain the user configuration
    for the OSIsoft <-> Flexy connection.

---
+++ OSIsoft_Demo/src/OSIsoftConfig.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * This class contains the configuration 
+ * for the Flexy <-> OSIsoft connection
+ * 
+ * HMS Industrial Networks Inc. Solution Center
+ * 
+ * @author thk
+ *
+ */
+public class OSIsoftConfig {
+
+}
