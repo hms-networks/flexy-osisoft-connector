6bc6feb8a82dcc07050a79e5059e014b3cb645fb
TomKimsey <thk@hms.se>
2018-04-25 10:54:00 -0400

    Added readFile method to OSIsoftServer.java

--- OSIsoft_Demo/src/OSIsoftServer.java
+++ OSIsoft_Demo/src/OSIsoftServer.java
@@ -1,3 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
+
 import com.ewon.ewonitf.EWException;
 import com.ewon.ewonitf.ScheduledActionManager;
 
@@ -52,4 +60,30 @@ public class OSIsoftServer {
       return jsonBody;
    }
    
+   // Reads a file and returns it as a string
+   public static String readFile(String path) {
+
+      FileConnection fconn = null;
+      InputStream is       = null;
+      int bufferSize       = 10000;
+
+      try {
+         fconn = (FileConnection) Connector.open(path);
+         // If no exception is thrown, then the URI is valid, but the file may or may not
+         // exist.
+
+         is = fconn.openInputStream();
+         int Read;
+         byte[] Buffer = new byte[bufferSize];
+
+         Read = is.read(Buffer);
+         fconn.close();
+         return new String(Buffer, 0, Read);
+
+      } catch (IOException ioe) {
+         System.out.println("Error: " + ioe.toString());
+         //Returns an empty string in error case
+         return "";
+      }
+   }
 }
