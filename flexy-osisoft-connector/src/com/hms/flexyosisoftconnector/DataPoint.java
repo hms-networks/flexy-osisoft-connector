1e8410cbbdb7764acc03304b6b3d9045c538b641
TomKimsey <thk@hms.se>
2018-10-24 18:01:34 -0400

    Created DataPoint.java

---
+++ flexy-osisoft-connector/src/com/hms/flexyosisoftconnector/DataPoint.java
@@ -0,0 +1,48 @@
+package com.hms.flexyosisoftconnector;
+import java.util.Date;
+
+/**
+ * DataPoint class
+ *
+ * Class object for a DataPoint.  This is a value and a time.
+ *
+ * HMS Industrial Networks Inc. Solution Center
+ *
+ * @author thk
+ *
+ */
+
+public class DataPoint {
+
+   private long valueLong;
+   private String timestamp;
+
+   public DataPoint(long value, String time)
+   {
+      valueLong = value;
+      timestamp = time;
+   }
+
+   public long getValueLong()
+   {
+      return valueLong;
+   }
+
+   public String getTimeStamp()
+   {
+      return timestamp;
+   }
+
+   public String toString()
+   {
+      String s = "";
+      s += valueLong + " " + timestamp;
+      return s;
+   }
+
+   public boolean equals(DataPoint p)
+   {
+      if (p.getTimeStamp().equals(timestamp) && p.getValueLong()==valueLong)return true;
+      return false;
+   }
+}
