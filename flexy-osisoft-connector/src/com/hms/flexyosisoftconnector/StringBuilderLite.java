7cca80b0adaa2a19f0102452cf9f47e74dd0ca0e
TomKimsey <thk@hms.se>
2018-10-30 16:09:41 -0400

    Created StringBuilderLite
    This is a mininmal implmentation of java.lang.StringBuilder which is not availible in java 1.4.  This allows for appending
    strings to a preallocated char array, minimizing the delays associated with memory allocation
    and garbage collection.

---
+++ flexy-osisoft-connector/src/com/hms/flexyosisoftconnector/StringBuilderLite.java
@@ -0,0 +1,53 @@
+package com.hms.flexyosisoftconnector;
+
+/**
+ * StringBuilderLite class
+ *
+ * Class for building large preallocated strings
+ *
+ * HMS Industrial Networks Inc. Solution Center
+ *
+ * @author thk
+ *
+ */
+
+import java.util.Arrays;
+
+public class StringBuilderLite {
+   private int size = 0;
+   private int index = 0;
+   private char[] strValue;
+
+   public StringBuilderLite(int numChars)
+   {
+      size = numChars;
+      strValue = new char[size];
+   }
+
+   public void clearString()
+   {
+      index = 0;
+   }
+
+   public String toString()
+   {
+      return String.valueOf(strValue, 0, index);
+   }
+
+   public boolean append(String s)
+   {
+      boolean retval = true;
+      if (s == null) s = "";
+      int len = s.length();
+      try
+      {
+         s.getChars(0, len, strValue, index);
+         index += len;
+      } catch (IndexOutOfBoundsException e)
+      {
+         Logger.LOG_EXCEPTION(e);
+         retval = false;
+      }
+      return retval;
+   }
+}
