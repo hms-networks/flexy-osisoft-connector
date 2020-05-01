1e293b7730722bf85df21da6cc93652de5878be7
oliver-walker-hms <olwa@hms.se>
2020-05-01 14:26:30 -0400

    changed all file formatting to google style

--- src/com/hms/flexyosisoftconnector/Main.java
+++ src/com/hms/flexyosisoftconnector/Main.java
@@ -1,4 +1,5 @@
 package com.hms.flexyosisoftconnector;
+
 import java.util.Date;
 
 import com.ewon.ewonitf.SysControlBlock;
@@ -6,202 +7,200 @@ import com.hms.flexyosisoftconnector.JSON.JSONException;
 
 /**
  * eWON Flexy java demo for OSIsoft Server
- *
- * This demo reads multiple tag values from an eWON Flexy IO Server and POSTs
- * them to an OSIsoft PI Server.
- *
+ * <p>
+ * This demo reads multiple tag values from an eWON Flexy IO Server and POSTs them to an OSIsoft PI
+ * Server.
+ * <p>
  * HMS Networks Inc. Solution Center
- *
  */
 public class Main {
 
-   /**
-    * Application Version Numbers
-    */
-   static final int MAJOR_VERSION = 1;
-   static final int MINOR_VERSION = 0;
+  /**
+   * Application Version Numbers
+   */
+  static final int MAJOR_VERSION = 1;
+  static final int MINOR_VERSION = 0;
 
-   /**
-    * Minimum amount of free memory before data trimming occurs
-    */
-   static final long MINIMUM_MEMORY = 5000000;
+  /**
+   * Minimum amount of free memory before data trimming occurs
+   */
+  static final long MINIMUM_MEMORY = 5000000;
 
-   /**
-    * Number of tags to trim when memory is low
-    */
-   static final int TAGS_TO_TRIM = 2;
+  /**
+   * Number of tags to trim when memory is low
+   */
+  static final int TAGS_TO_TRIM = 2;
 
-   /**
-    * Default name of a new Ewon
-    */
-   static final String DEFAULT_EWON_NAME = "eWON";
+  /**
+   * Default name of a new Ewon
+   */
+  static final String DEFAULT_EWON_NAME = "eWON";
 
-   /**
-    * Filename of connector config file
-    */
-    static final String CONNECTOR_CONFIG_FILENAME = "/usr/ConnectorConfig.json";
+  /**
+   * Filename of connector config file
+   */
+  static final String CONNECTOR_CONFIG_FILENAME = "/usr/ConnectorConfig.json";
 
-   /**
-    * Configuration of PI connector
-    */
-   static OSIsoftConfig piConfig;
+  /**
+   * Configuration of PI connector
+   */
+  static OSIsoftConfig piConfig;
 
-   /**
-    * PI Server management object
-    */
-   static OSIsoftServer piServer;
+  /**
+   * PI Server management object
+   */
+  static OSIsoftServer piServer;
 
-   /**
-    * Current available JVM memory
-    */
-   static long AvailableMemory;
+  /**
+   * Current available JVM memory
+   */
+  static long AvailableMemory;
 
 
-   public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      // Time keeping variables
-      long lastUpdateTimeMs = 0;
-      long currentTimeMs;
+    // Time keeping variables
+    long lastUpdateTimeMs = 0;
+    long currentTimeMs;
 
-      //Start the webserver to accept json file
-      RestFileServer restServer = new RestFileServer();
-      restServer.start();
+    //Start the webserver to accept json file
+    RestFileServer restServer = new RestFileServer();
+    restServer.start();
 
-      //Unique name of the flexy
-      String flexyName = getFlexyName();
+    //Unique name of the flexy
+    String flexyName = getFlexyName();
 
-      // Indicate the version number and that the application is starting
-      Logger.LOG_CRITICAL("OSIsoft Connector v" + MAJOR_VERSION + "." + MINOR_VERSION + " starting");
+    // Indicate the version number and that the application is starting
+    Logger.LOG_CRITICAL("OSIsoft Connector v" + MAJOR_VERSION + "." + MINOR_VERSION + " starting");
 
-      //Check that the flexy has a non-default name, stop the application if not
-      if (flexyName.equals(DEFAULT_EWON_NAME))
-      {
-         Logger.LOG_ERR("Device name is set to \"eWON\" which is the default name");
-         Logger.LOG_ERR("This device's name must be changed from default");
-         Logger.LOG_ERR("Application aborting due to default name use");
-         System.exit(0);
-      }
+    //Check that the flexy has a non-default name, stop the application if not
+    if (flexyName.equals(DEFAULT_EWON_NAME)) {
+      Logger.LOG_ERR("Device name is set to \"eWON\" which is the default name");
+      Logger.LOG_ERR("This device's name must be changed from default");
+      Logger.LOG_ERR("Application aborting due to default name use");
+      System.exit(0);
+    }
 
+    try {
+      piConfig = new OSIsoftConfig(CONNECTOR_CONFIG_FILENAME);
+    } catch (JSONException e) {
+      Logger.LOG_ERR(CONNECTOR_CONFIG_FILENAME + " is malformed");
+      Logger.LOG_EXCEPTION(e);
+      System.exit(0);
+    }
+
+    // Set the path to the directory holding the certificate for the server
+    // Only needed if the certificate is self signed
+    setCertificatePath(piConfig.getCertificatePath());
+    setHttpTimeouts();
+
+    int res = OSIsoftServer.NO_ERROR;
+    piServer = new OSIsoftServer(piConfig.getServerIP(), piConfig.getServerLogin(),
+        piConfig.getServerWebID(), flexyName);
+
+    do {
       try {
-         piConfig = new OSIsoftConfig(CONNECTOR_CONFIG_FILENAME);
+        res = piServer
+            .initTags(piConfig.getServerIP(), piConfig.getTags(), piConfig.getCommunicationType());
       } catch (JSONException e) {
-         Logger.LOG_ERR(CONNECTOR_CONFIG_FILENAME + " is malformed");
-         Logger.LOG_EXCEPTION(e);
-         System.exit(0);
+        Logger.LOG_ERR("Linking eWON tags to OSIsoft PI server failed");
+        Logger.LOG_EXCEPTION(e);
+      }
+    } while (res != OSIsoftServer.NO_ERROR);
+
+    DataPoster datathread = new DataPoster(piConfig.getCommunicationType());
+    datathread.start();
+
+    // Infinite loop
+    while (true) {
+
+      AvailableMemory = Runtime.getRuntime().freeMemory();
+      currentTimeMs = System.currentTimeMillis();
+
+      if ((currentTimeMs - lastUpdateTimeMs) >= piConfig.getCycleTimeMs()) {
+
+        // Update the last update time
+        lastUpdateTimeMs = currentTimeMs;
+
+        String time = piServer.convertTimeString(new Date());
+        for (int i = 0; i < piConfig.getTags().size(); i++) {
+          if (AvailableMemory < MINIMUM_MEMORY) {
+            ((Tag) piConfig.getTags().get(i)).trimOldestEntries(TAGS_TO_TRIM);
+          }
+          ((Tag) piConfig.getTags().get(i)).recordTagValue(time);
+        }
+
+        Thread.yield();
+      }
+    }
+  }
+
+  /**
+   * Reads the unique name given to the flexy
+   *
+   * @return Name of the Flexy
+   */
+  private static String getFlexyName() {
+    String res = "";
+    SysControlBlock SCB;
+    try {
+      SCB = new SysControlBlock(SysControlBlock.SYS);
+      res = SCB.getItem("Identification");
+    } catch (Exception e) {
+      Logger.LOG_ERR("Error reading eWON's name");
+    }
+    return res;
+  }
+
+  /**
+   * Sets the directory that the eWON uses to check for SSL Certificates
+   *
+   * @param path directory path where certificate is stored
+   */
+  private static void setCertificatePath(String path) {
+
+    SysControlBlock SCB;
+    try {
+      SCB = new SysControlBlock(SysControlBlock.SYS);
+      if (!SCB.getItem("HttpCertDir").equals(path)) {
+        SCB.setItem("HttpCertDir", path);
+        SCB.saveBlock(true);
+      }
+    } catch (Exception e) {
+      Logger.LOG_ERR("Setting certificate directory failed");
+      System.exit(0);
+    }
+  }
+
+
+  /**
+   * Sets the http timeouts Note: This changes the eWON's global HTTP timeouts and stores these
+   * values in NV memory.
+   */
+  private static void setHttpTimeouts() {
+    SysControlBlock SCB;
+    boolean needsSave = false;
+    final String HTTPS_TIMEOUT_S = "2";
+    try {
+      SCB = new SysControlBlock(SysControlBlock.SYS);
+
+      if (!SCB.getItem("HTTPC_SDTO").equals(HTTPS_TIMEOUT_S)) {
+        SCB.setItem("HTTPC_SDTO", HTTPS_TIMEOUT_S);
+        needsSave = true;
       }
 
-      // Set the path to the directory holding the certificate for the server
-      // Only needed if the certificate is self signed
-      setCertificatePath(piConfig.getCertificatePath());
-      setHttpTimeouts();
-
-      int res = OSIsoftServer.NO_ERROR;
-      piServer = new OSIsoftServer(piConfig.getServerIP(), piConfig.getServerLogin(), piConfig.getServerWebID(), flexyName);
-
-      do {
-         try {
-            res = piServer.initTags(piConfig.getServerIP(), piConfig.getTags(), piConfig.getCommunicationType());
-         } catch (JSONException e) {
-            Logger.LOG_ERR("Linking eWON tags to OSIsoft PI server failed");
-            Logger.LOG_EXCEPTION(e);
-         }
-      } while (res != OSIsoftServer.NO_ERROR);
-
-      DataPoster datathread = new DataPoster(piConfig.getCommunicationType());
-      datathread.start();
-
-      // Infinite loop
-      while (true) {
-
-         AvailableMemory = Runtime.getRuntime().freeMemory();
-         currentTimeMs = System.currentTimeMillis();
-
-         if ((currentTimeMs - lastUpdateTimeMs) >= piConfig.getCycleTimeMs()) {
-
-            // Update the last update time
-            lastUpdateTimeMs = currentTimeMs;
-
-            String time = piServer.convertTimeString(new Date());
-            for (int i = 0; i < piConfig.getTags().size(); i++) {
-               if(AvailableMemory < MINIMUM_MEMORY)
-               {
-                  ((Tag) piConfig.getTags().get(i)).trimOldestEntries(TAGS_TO_TRIM);
-               }
-               ((Tag) piConfig.getTags().get(i)).recordTagValue(time);
-            }
-
-            Thread.yield();
-         }
+      if (!SCB.getItem("HTTPC_RDTO").equals(HTTPS_TIMEOUT_S)) {
+        SCB.setItem("HTTPC_RDTO", HTTPS_TIMEOUT_S);
+        needsSave = true;
       }
-   }
 
-   /**
-    * Reads the unique name given to the flexy
-    * @return Name of the Flexy
-    */
-   private static String getFlexyName()
-   {
-      String res = "";
-      SysControlBlock SCB;
-      try {
-         SCB = new SysControlBlock(SysControlBlock.SYS);
-         res = SCB.getItem("Identification");
-      } catch (Exception e) {
-         Logger.LOG_ERR("Error reading eWON's name");
-      }
-      return res;
-   }
+       if (needsSave) {
+          SCB.saveBlock(true);
+       }
 
-   /**
-    * Sets the directory that the eWON uses to check for SSL Certificates
-    * @param path directory path where certificate is stored
-    */
-   private static void setCertificatePath(String path) {
-
-      SysControlBlock SCB;
-      try {
-         SCB = new SysControlBlock(SysControlBlock.SYS);
-         if(!SCB.getItem("HttpCertDir").equals(path))
-         {
-            SCB.setItem("HttpCertDir", path);
-            SCB.saveBlock(true);
-         }
-      } catch (Exception e) {
-         Logger.LOG_ERR("Setting certificate directory failed");
-         System.exit(0);
-      }
-   }
-
-
-   /**
-    * Sets the http timeouts
-    * Note: This changes the eWON's global HTTP timeouts and stores
-    *       these values in NV memory.
-    */
-   private static void setHttpTimeouts() {
-      SysControlBlock SCB;
-      boolean needsSave = false;
-      final String HTTPS_TIMEOUT_S = "2";
-      try {
-         SCB = new SysControlBlock(SysControlBlock.SYS);
-
-         if (!SCB.getItem("HTTPC_SDTO").equals(HTTPS_TIMEOUT_S))
-         {
-            SCB.setItem("HTTPC_SDTO", HTTPS_TIMEOUT_S);
-            needsSave = true;
-         }
-
-         if (!SCB.getItem("HTTPC_RDTO").equals(HTTPS_TIMEOUT_S))
-         {
-            SCB.setItem("HTTPC_RDTO", HTTPS_TIMEOUT_S);
-            needsSave = true;
-         }
-
-         if (needsSave) SCB.saveBlock(true);
-
-      } catch (Exception e) {
-         Logger.LOG_ERR("Setting timeouts failed. Application ending");
-         System.exit(0);
-      }
-   }
+    } catch (Exception e) {
+      Logger.LOG_ERR("Setting timeouts failed. Application ending");
+      System.exit(0);
+    }
+  }
 }
--- src/com/hms/flexyosisoftconnector/OSIsoftConfig.java
+++ src/com/hms/flexyosisoftconnector/OSIsoftConfig.java
@@ -1,152 +1,149 @@
 package com.hms.flexyosisoftconnector;
+
 import java.util.ArrayList;
 import com.hms.flexyosisoftconnector.JSON.*;
 
 /**
- *
- * This class contains the configuration
- * for the Flexy <-> OSIsoft connection
- *
+ * This class contains the configuration for the Flexy <-> OSIsoft connection
+ * <p>
  * HMS Networks Inc. Solution Center
- *
  */
 public class OSIsoftConfig {
 
-    // IP address of OSIsoft PI Server
-    String piServerIP;
+  // IP address of OSIsoft PI Server
+  String piServerIP;
 
-    // WebID of the OSIsoft PI Dataserver
-    String dataServerWebID;
+  // WebID of the OSIsoft PI Dataserver
+  String dataServerWebID;
 
-    // Your BASE64 encoded BASIC authentication credentials
-    // Visit https://www.base64encode.org/
-    // Encode: "username:password"
-    String piServerLogin;
+  // Your BASE64 encoded BASIC authentication credentials
+  // Visit https://www.base64encode.org/
+  // Encode: "username:password"
+  String piServerLogin;
 
-    // Path to the directory containing your OSIsoft server's certificate
-    String eWONCertificatePath;
+  // Path to the directory containing your OSIsoft server's certificate
+  String eWONCertificatePath;
 
-    // Update rate for all tags in milliseconds
-    int cycleTimeMs;
+  // Update rate for all tags in milliseconds
+  int cycleTimeMs;
 
-    // String converted to int for use in switch statement
-    // Either (0) pre2019 for piwebapi or (1) post2019 for OMF
-    int communicationType;
+  // String converted to int for use in switch statement
+  // Either (0) pre2019 for piwebapi or (1) post2019 for OMF
+  int communicationType;
 
-    // Strings you can get off of the json file
-    String pre2019 = "piwebapi";
-    String post2019 = "omf";
-    // Strings set by web page config tool
-    String secondPre2019 = "PI Web API 2018 and older";
-    String secondPost2019 = "PI Web API 2019+";
+  // Strings you can get off of the json file
+  String pre2019 = "piwebapi";
+  String post2019 = "omf";
+  // Strings set by web page config tool
+  String secondPre2019 = "PI Web API 2018 and older";
+  String secondPost2019 = "PI Web API 2019+";
 
-    // Expose these for switch statement conditionals
-    public static final int piwebapi = 0;
-    public static final int omf = 1;
+  // Expose these for switch statement conditionals
+  public static final int piwebapi = 0;
+  public static final int omf = 1;
 
-    // List of tags
+  // List of tags
 
-    public static ArrayList tags = new ArrayList();
+  public static ArrayList tags = new ArrayList();
 
 
-    // Build the OSIsoft config from a configuration json file
-    public OSIsoftConfig(String configFile) throws JSONException {
+  // Build the OSIsoft config from a configuration json file
+  public OSIsoftConfig(String configFile) throws JSONException {
 
-       //Read in the JSON file to a string
-       JSONTokener JsonT = new JSONTokener(FileReader.readFile("file://" + configFile));
+    //Read in the JSON file to a string
+    JSONTokener JsonT = new JSONTokener(FileReader.readFile("file://" + configFile));
 
-       //Build a JSON Object containing the whole file
-       JSONObject configJSON = new JSONObject(JsonT);
+    //Build a JSON Object containing the whole file
+    JSONObject configJSON = new JSONObject(JsonT);
 
-       //Build a JSON Object containing the "ServerConfig"
-       JSONObject serverConfig = configJSON.getJSONObject("ServerConfig");
+    //Build a JSON Object containing the "ServerConfig"
+    JSONObject serverConfig = configJSON.getJSONObject("ServerConfig");
 
-       //Set the server config parameters
-       piServerIP = serverConfig.getString("IP");
-       dataServerWebID = serverConfig.getString("WebID");
-       piServerLogin = serverConfig.getString("Credentials");
+    //Set the server config parameters
+    piServerIP = serverConfig.getString("IP");
+    dataServerWebID = serverConfig.getString("WebID");
+    piServerLogin = serverConfig.getString("Credentials");
 
-       //Build a JSON Object containing the "eWONConfig"
-       JSONObject eWONConfig = configJSON.getJSONObject("eWONConfig");
-       eWONCertificatePath = eWONConfig.getString("CertificatePath");
+    //Build a JSON Object containing the "eWONConfig"
+    JSONObject eWONConfig = configJSON.getJSONObject("eWONConfig");
+    eWONCertificatePath = eWONConfig.getString("CertificatePath");
 
-       //Build a JSON Object containing the "AppConfig"
-       JSONObject appConfig = configJSON.getJSONObject("AppConfig");
-       cycleTimeMs = appConfig.getInt("CycleTimeMs");
+    //Build a JSON Object containing the "AppConfig"
+    JSONObject appConfig = configJSON.getJSONObject("AppConfig");
+    cycleTimeMs = appConfig.getInt("CycleTimeMs");
 
-       String tmpCommunicationType = appConfig.getString("CommunicationType");
-       if(tmpCommunicationType.equalsIgnoreCase(pre2019) || tmpCommunicationType.equalsIgnoreCase(secondPre2019)) {
-           communicationType = piwebapi;
-       } else if (tmpCommunicationType.equalsIgnoreCase(post2019) || tmpCommunicationType.equalsIgnoreCase(secondPost2019)) {
-           communicationType = omf;
-       } else {
-           // can't do anything without a valid communication type..
-           Logger.LOG_ERR("Invalid communication type in config json");
-           Logger.LOG_ERR("Change the communication Type in config json to one of the valid options in the readme and restart the connector.");
-           Logger.LOG_ERR("OSIsoft connector Shutting down.");
-           System.exit(1);
-       }
-
-       boolean shouldLogDuplicateValues = appConfig.getBoolean("PostDuplicateTagValues");
-
-       if(appConfig.has("LoggingLevel"))
-       {
-          boolean res = Logger.SET_LOG_LEVEL(appConfig.getInt("LoggingLevel"));
-          if (!res)
-          {
-             Logger.LOG_ERR("Invalid logging level specified");
-          }
-       }
-
-       //Build a JSON Array containing the tag names
-       JSONArray tagNames = configJSON.getJSONArray("TagList");
-
-       //For each tagname in the config file create a tag and add it to
-       //the arraylist of tags
-       for(int i = 0; i < tagNames.length(); i++) {
-          Tag tag = new Tag(tagNames.getString(i), shouldLogDuplicateValues);
-          if (tag.isValidTag())
-          {
-             tags.add(tag);
-          } else
-          {
-             Logger.LOG_ERR("Tag \"" + tag.getTagName() + "\" does not exist on this eWON");
-          }
-       }
+    String tmpCommunicationType = appConfig.getString("CommunicationType");
+    if (tmpCommunicationType.equalsIgnoreCase(pre2019) || tmpCommunicationType
+        .equalsIgnoreCase(secondPre2019)) {
+      communicationType = piwebapi;
+    } else if (tmpCommunicationType.equalsIgnoreCase(post2019) || tmpCommunicationType
+        .equalsIgnoreCase(secondPost2019)) {
+      communicationType = omf;
+    } else {
+      // can't do anything without a valid communication type..
+      Logger.LOG_ERR("Invalid communication type in config json");
+      Logger.LOG_ERR(
+          "Change the communication Type in config json to one of the valid options in the readme and restart the connector.");
+      Logger.LOG_ERR("OSIsoft connector Shutting down.");
+      System.exit(1);
     }
 
-    // Returns the IP Address of the OSIsoft Server
-    public String getServerIP() {
-       return piServerIP;
+    boolean shouldLogDuplicateValues = appConfig.getBoolean("PostDuplicateTagValues");
+
+    if (appConfig.has("LoggingLevel")) {
+      boolean res = Logger.SET_LOG_LEVEL(appConfig.getInt("LoggingLevel"));
+      if (!res) {
+        Logger.LOG_ERR("Invalid logging level specified");
+      }
     }
 
-    // Returns the BASE64 encoded authentication credentials
-    public String getServerLogin() {
-       return piServerLogin;
-    }
+    //Build a JSON Array containing the tag names
+    JSONArray tagNames = configJSON.getJSONArray("TagList");
 
-    // Returns the Web ID of the dataserver
-    public String getServerWebID() {
-       return dataServerWebID;
+    //For each tagname in the config file create a tag and add it to
+    //the arraylist of tags
+    for (int i = 0; i < tagNames.length(); i++) {
+      Tag tag = new Tag(tagNames.getString(i), shouldLogDuplicateValues);
+      if (tag.isValidTag()) {
+        tags.add(tag);
+      } else {
+        Logger.LOG_ERR("Tag \"" + tag.getTagName() + "\" does not exist on this eWON");
+      }
     }
+  }
 
-    //Returns the list of tags
-    public ArrayList getTags() {
-       return tags;
-    }
+  // Returns the IP Address of the OSIsoft Server
+  public String getServerIP() {
+    return piServerIP;
+  }
 
-    // Returns the path to the OSIsoft server's certificate
-    public String getCertificatePath() {
-       return eWONCertificatePath;
-    }
+  // Returns the BASE64 encoded authentication credentials
+  public String getServerLogin() {
+    return piServerLogin;
+  }
 
-    // Returns the cycle time in milliseconds
-    public int getCycleTimeMs() {
-       return cycleTimeMs;
-    }
+  // Returns the Web ID of the dataserver
+  public String getServerWebID() {
+    return dataServerWebID;
+  }
 
-    public int getCommunicationType() {
-       return communicationType;
-    }
+  //Returns the list of tags
+  public ArrayList getTags() {
+    return tags;
+  }
+
+  // Returns the path to the OSIsoft server's certificate
+  public String getCertificatePath() {
+    return eWONCertificatePath;
+  }
+
+  // Returns the cycle time in milliseconds
+  public int getCycleTimeMs() {
+    return cycleTimeMs;
+  }
+
+  public int getCommunicationType() {
+    return communicationType;
+  }
 
 }
