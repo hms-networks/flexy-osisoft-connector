c4c7403dc70c3c0c4775e86b028c4b89a8ec1645
oliver-walker-hms <olwa@hms.se>
2020-05-04 18:52:29 -0400

    fixed imports to rest file server

--- src/com/hms/flexyosisoftconnector/RestFileServer.java
+++ src/com/hms/flexyosisoftconnector/RestFileServer.java
@@ -1,5 +1,6 @@
 package com.hms.flexyosisoftconnector;
 
+import com.hms_networks.americas.sc.logging.Logger;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
