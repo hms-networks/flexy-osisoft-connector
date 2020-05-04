5fa8c386258d6d7a282e1d0d1a196f4524c853dc
oliver-walker-hms <olwa@hms.se>
2020-05-04 16:43:14 -0400

    added Tag Payload Class

---
+++ src/com/hms/flexyosisoftconnector/TagPayload.java
@@ -0,0 +1,59 @@
+package com.hms.flexyosisoftconnector;
+
+import com.hms_networks.americas.sc.datapoint.DataPoint;
+import com.hms_networks.americas.sc.string.PreAllocatedStringBuilder;
+
+/**
+ * This class holds the incomplete payload as well as one of the datapoint objects.
+ *
+ * @author HMS Networks Inc. Solution Center
+ */
+public class TagPayload {
+
+  /** Holds the JSON payload. */
+  private PreAllocatedStringBuilder payload;
+
+  /** Holds the tag information associated with the payload. */
+  private DataPoint dataPoint;
+
+  /** Constructs a new empty payload. */
+  public TagPayload() {
+    payload = new PreAllocatedStringBuilder(OsisoftJsonPayload.MAX_PAYLOAD_NUM_CHARACTERS);
+  }
+
+  /**
+   * Appends payloadPart to the already constructed payload.
+   *
+   * @param payloadPart a JSON segment to append to the payload
+   */
+  public void add(String payloadPart) {
+    payload.append(payloadPart);
+  }
+
+  /**
+   * Get the JSON payload.
+   *
+   * @return the JSON payload
+   */
+  public String getPayload() {
+    return payload.toString();
+  }
+
+  /**
+   * Get the datapoint.
+   *
+   * @return the datapoint
+   */
+  public DataPoint getDataPoint() {
+    return dataPoint;
+  }
+
+  /**
+   * Set the datapoint
+   *
+   * @param dataPoint holds the datapoint to set.
+   */
+  public void setDataPoint(DataPoint dataPoint) {
+    this.dataPoint = dataPoint;
+  }
+}
