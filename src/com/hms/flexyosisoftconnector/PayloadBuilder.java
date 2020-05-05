68115adfa9b33e37866a9e0efe1b6d9020aadbe0
oliver-walker-hms <olwa@hms.se>
2020-05-05 15:34:38 -0400

    ensure initialized type

--- src/com/hms/flexyosisoftconnector/PayloadBuilder.java
+++ src/com/hms/flexyosisoftconnector/PayloadBuilder.java
@@ -340,7 +340,7 @@ public class PayloadBuilder {
    * @return Returns a string representation of the constructed json body.
    */
   public static String buildNewPointBody(TagInfo tag) {
-    String type;
+    String type = "null";
     String jsonBody;
 
     if (tag.getType().equals(TagType.BOOLEAN)) {
